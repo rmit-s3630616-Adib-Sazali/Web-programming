38af9c3470e215d1c4273d3640902bc0fc72fb51
rmit-s3630616-Adib-Sazali <30641146+rmit-s3630616-Adib-Sazali@users.noreply.github.com>
2017-10-02 15:21:24 +0800

    test
    test

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+ <head>
+    <link href="stylesheet.css" rel="stylesheet">
+    
+    </head>
+  
+	
+	<body>
+        
+            
+        
+        
+        
+        <div class="topBanner">
+        <img src="../img/logoFin.png" width=640 height=400 alt='Silverado logo' />
+        </div>
+        
+        
+		<nav>
+			<ul>
+				<li><a href="../a3/index.php" target="_blank">Home</a></li>
+				<li><a href="../a3/showing.php" target="_blank">Now Showing</a></li>
+			</ul>	
+		</nav>
+        
+        
+        
+		<main>
+            
+           
+				<h1>We're Back!</h1>
+				<p>Silverado has gotten a facelift and we are readying ourselves for our grand re-opening!
+				we hope you will join us Friday 13th of August for the fun! There will be a special screening
+				of classic movies and free popcorn! Bring your family and friends as it will be a night out to
+				remember. Remember to book tickets for the opening night screenings as they will sell out fast.
+				
+				We look forward to you joining the Silverado family</p>
+           
+			
+				<h1>Whats New?</h1>
+				<p class = "bodyText">With our renovations underway we are excited to announce all the new additions to our beloved
+				Silverado Cinema! We are bringing you a brand new cinema experience with upgrades and additions
+				to our seating plan, normal seats will still be available but beanbag chairs for couples and
+				families has been added, coupled with the coveted new first class lounge seats that will make
+				the cinema experience one of even greater luxury.
+				
+				We hope you will join us come our re-opening night</p>
+            
+				<h1>Whats that Sound?</h1>
+				<p class = "bodyText">Included in our upcoming renovations are plans new for Dolby Sound Systems.
+				State of the art technology, these new systems will help create a more immersive, more impactful 
+				viewing experience.
+				
+				Also making an appearance will be 3D capable projectors for certain 3D sessions, be sure to find
+				out firsthand what all the new tech ammounts to at our grand re-opening!</p>
+           
+           
+		</main>
+        
+	<footer>
+		
+       
+			 &copy; Adib Sazali (s3630616) & &copy; Maxwell (s3668048)
+             <script>
+        document.write(new Date().getFullYear());
+      </script>.
+      All rights reserved. 
+	</footer>	
+
+	</body>
+</html>
